10ce2f0c45037a18077d3b71831daa1afabca9e1
Ratan Phayade <ratanphayade@users.noreply.github.com>
2024-03-15 15:49:14 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -101,6 +101,7 @@ async function run(): Promise<void> {
   }
 
   try {
+    console.log(process.env.GITHUB_EVENT_NAME);
     // check if the event is pull_request
     if (
       process.env.GITHUB_EVENT_NAME === 'pull_request' ||
